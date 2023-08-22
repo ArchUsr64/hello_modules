1d9aaba7a85c6f013f98350cc1fa08afbe7a19b5
Anshul <anshulusr@gmail.com>
2023-08-22 12:10:37 +0530

    Added `printk` logging on init and exit

--- hello.c
+++ hello.c
@@ -1,8 +1,12 @@
 #include <linux/module.h>
+#include <linux/printk.h>
 
-int start(void) { return 0; }
+int start(void) {
+  pr_info("Hello, World!\n");
+  return 0;
+}
 
-void cleanup(void) {}
+void cleanup(void) { pr_info("Goodbye, World!\n"); }
 
 module_init(start);
 module_exit(cleanup);
